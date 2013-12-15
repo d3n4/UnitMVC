d55d4029ea6f5d47c5a2b48908c3d99b9aa0a433
d3n4 <id110099122@gmail.com>
2013-12-15 06:42:14 +0200

    removed observe demo

--- applications/Observer/observe/MainPage.php
+++
@@ -1,9 +0,0 @@
-<?php
-    class MainPage extends OBModel {
-        public $username = "No name";
-        public $last_update;
-
-        public function MainPage() {
-            $this->last_update = time();
-        }
-    }
